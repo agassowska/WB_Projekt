fce1baa643bc1df9f67cad21feac3ea5285edf07
Elzbieta Jowik <elzbietajowik@gmail.com>
2020-03-27 10:21:32 +0100

    update

--- Prezentacja/skrypt.R
+++ Prezentacja/skrypt.R
@@ -3,19 +3,36 @@ library(FactoMineR)
 
 # Metoda PCA dla zbioru z brakami
 data("airquality")
-## 1. estimate the number of dimensions used in the reconstruction formula 
+
+# IMPUTATION
+## Step 1. estimate the number of dimensions used in the reconstruction formula 
 nb = estim_ncpPCA(airquality, ncp.max=5)
-## 2. impute the data set with the impute.PCA function using the number 
+# Important arguments:
+   # ncp.min
+   # ncp.max
+   # method
+   # method.cv
+   # pNA (for Kfold method.cv)
+   # nbsim (for Kfold method.cv)
+
+## Step 2. impute the data set with the impute.PCA function using the number 
 ##    of dimensions previously calculated
-res.comp = imputePCA(airquality,ncp=2)
+res.comp = imputePCA(airquality,ncp=2, method = 'Regularized', seed = 123)
+# Important arguments:
+    # ncp
+    # method
+
+# METHOD PERFORMANCE
 ## 3. perform the PCA on the completed data set using the PCA function 
 ## of the FactoMineR package
 res.pca = PCA(res.comp$completeObs) 
 
-# Visualising uncertaintity due to missing data
+
+# VISUALISING UNCERTAINITY DUE TO MISSING DATA
 res.mipca <- MIPCA(airquality, ncp = 2, nboot = 100)
 plot.MIPCA(res.mipca, choice = 'dim')
 plot.MIPCA(res.mipca, 'var')
 
 # Przydatny link:
 ## http://factominer.free.fr/course/missing.html
+?imputePCA
