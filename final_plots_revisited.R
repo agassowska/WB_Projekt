af88d64121d8e7f140b7327b5f1c12b161cdf635
PlentyCoups <matis33pl@gmail.com>
2020-06-01 18:17:37 +0200

    article fixes

--- final_plots_revisited.R
+++ final_plots_revisited.R
@@ -10,6 +10,7 @@ test_data <- read.csv('./test_data/test_data.csv')
 measures_plot_data <- melt(test_data)
 
 measures_subplot <- ggplot(measures_plot_data, aes(x=dataset, color=package, y=value)) +
+  geom_col(aes(x=dataset, y=1), fill='black', alpha=.1, color=NA) +
   geom_point(size=2, position=position_dodge(width=.75)) +
   ylim(c(0, 1)) +
   xlab('') +
@@ -22,12 +23,12 @@ measures_plot <- measures_subplot +
   facet_grid(rows = vars(variable), cols = vars(model), switch='y') +
   labs(title='Measure value (on test subset) by package and dataset for each measure/model combination',
        subtitle='higher is better') +
-  theme(plot.title=element_text(size=16),
-        plot.subtitle=element_text(size=14),
-        strip.text=element_text(size=12),
-        legend.title=element_text(size=12),
-        legend.text=element_text(size=10),
-        axis.text=element_text(size=10))
+  theme(plot.title=element_text(size=24),
+        plot.subtitle=element_text(size=20),
+        strip.text=element_text(size=16),
+        legend.title=element_text(size=16),
+        legend.text=element_text(size=14),
+        axis.text=element_text(size=14))
 
 measures_plot
 
@@ -50,19 +51,23 @@ rankings_subplot <- ggplot(rankings_plot_data, aes(y=value, x=package)) +
   stat_summary(fun=mean, geom="errorbar", aes(ymax = ..y.., ymin = ..y..), width=.75, color='red') +
   stat_summary(fun=mean, geom="label", aes(label=round(..y.., digits=1)), color='red', alpha=0.75) +
   xlab('') +
-  ylab('')
+  ylab('') +
+  theme(
+    axis.text.x=element_text(angle=45, vjust=0.5)
+  )
+
 
 rankings_plot <- rankings_subplot +
   facet_grid(rows = vars(variable), cols = vars(model), switch='y') +
   labs(title='Ranked measure value (on test subset) by package for each measure/model combination',
        subtitle='lower is better', caption='Each red point represents ranked position on particular dataset.\nEach red line and number represent mean rank.') +
-  theme(plot.title=element_text(size=16),
-        plot.subtitle=element_text(size=14),
-        strip.text=element_text(size=12),
-        legend.title=element_text(size=12),
-        legend.text=element_text(size=10),
-        axis.text=element_text(size=10),
-        plot.caption=element_text(size=10, color='red'))
+  theme(plot.title=element_text(size=24),
+        plot.subtitle=element_text(size=20),
+        strip.text=element_text(size=16),
+        legend.title=element_text(size=16),
+        legend.text=element_text(size=14),
+        axis.text=element_text(size=14),
+        plot.caption=element_text(size=14, color='red'))
 
 rankings_plot
   
@@ -80,8 +85,13 @@ time_subplot_1 <- ggplot(time_data, aes(x=package, y=imputation_time)) +
   ylab('log(imputation time) (in seconds)') +
   scale_y_log10() +
   labs(title='by package') +
+  xlab('') +
   theme(
-    legend.position='none'
+    legend.position='none',
+    plot.title=element_text(size=20),
+    axis.text.y=element_text(size=14),
+    axis.text.x=element_text(size=14, angle=45, vjust=0.5),
+    axis.title.y=element_text(size=16)
   )
 
 time_subplot_2 <- ggplot(time_data, aes(x=dataset, color=package, y=imputation_time)) +
@@ -89,15 +99,19 @@ time_subplot_2 <- ggplot(time_data, aes(x=dataset, color=package, y=imputation_t
   ylab('log(imputation time) (in seconds)') +
   scale_y_log10() +
   labs(title='by package and dataset') +
+  xlab('') +
   theme(
-    axis.text.x=element_text(angle=45, vjust=0.5)
+    plot.title=element_text(size=20),
+    axis.text.y=element_text(size=14),
+    axis.text.x=element_text(size=14, angle=45, vjust=0.5),
+    axis.title.y=element_text(size=16),
+    legend.title=element_text(size=16),
+    legend.text=element_text(size=14)
   )
 
 times_plot <- time_subplot_1 / time_subplot_2 + 
   plot_annotation(title='Imputation time') +
-  theme(plot.title=element_text(size=16),
-        plot.subtitle=element_text(size=14),
-        legend.title=element_text(size=12),
-        legend.text=element_text(size=10),
-        axis.text=element_text(size=10))
+  theme(
+    plot.title=element_text(size=24)
+  )
 times_plot
